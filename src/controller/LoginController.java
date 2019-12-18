8c7abc4e33ca14909de2ae57a4209b69a0e610a3
huongthanhcsm <50436346+huongthanhcsm@users.noreply.github.com>
2019-12-18 16:47:07 +0700

    Add files via upload

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import Models.Account;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,15 +9,17 @@ import java.sql.Statement;
 import Models.Connect;
 
 public class LoginController {
-	public static boolean checkAccount(String txtAccount, String txtPassword) throws SQLException {
-		Statement statement = Connect.getConnection().createStatement();
+	public static Account checkAccount(String txtAccount, String txtPassword) throws SQLException {
+                Statement statement = Connect.getConnection().createStatement();
 		String sql = "select * from account where UserName = \'" + txtAccount + "\' and PassWord = \'" + txtPassword + "\'";
 		ResultSet resultSet = statement.executeQuery(sql);
 		try {
-			if (resultSet.next()) return true;
+			if (resultSet.next()){
+                            return(new Account(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getInt(4)));
+                        };
 		} finally {
 			Connect.close();
 		}
-		return false;
+		return null;
 	}
 }
